65318f0a89f6576c142a3c16dc6de8d1af0bedae
cyberzac <zac@cyberzac.se>
2013-10-15 23:49:56 +0200

    Revert "Added pywallet"
    This reverts commit f250235444524ed0079eac553a989b03c32938fe.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,7 +13,7 @@ then
   exit
 fi
 
-packages="grc coreutils spark maven nmap scala sbt git tomcat blueutil giter8 graphviz subsversion python python3 "
+packages="grc coreutils spark maven nmap scala sbt git tomcat blueutil giter8 graphviz subsversion python python3 wget"
 # Install homebrew packages
 brew install $packages
 # Upgrade
