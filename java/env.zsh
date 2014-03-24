7026ac4f90e5aabae3950fd347cae573854f1397
cyberzac <zac@cyberzac.se>
2014-03-24 16:01:53 +0100

    New java, emacs and bash stuff

--- java/env.zsh
+++ java/env.zsh
@@ -1,1 +1,2 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
