70f2a76bb261de322ceff51de91cb0cc5b82481f
Martin Zachrison <zac@cyberzac.se>
2014-08-25 14:09:15 +0200

    Bitocin stuff

---
+++ bitcoin/alias.zsh
@@ -0,0 +1,7 @@
+#
+# run bitcoin core on testnet
+alias bitcoind-regtest="open -a Bitcoin-Qt --args -regtest"
+alias bitcoind-testnet="open -a Bitcoin-Qt --args -testnet"
+alias bitcoind-testnet-gen="open -a Bitcoin-Qt --args -testnet -gen"
+alias bitcoind="open -a Bitcoin-Qt"
+
