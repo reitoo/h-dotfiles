1110625b41a9242e0ec33ba60b3393c6b18558ee
reitoo <rein@toompuu.se>
2014-08-28 23:06:02 +0200

    my own aliases

---
+++ rein/alias.zsh
@@ -0,0 +1,5 @@
+alias macmini="ssh -p 443 rein.nu -L 1176:localhost:1176 -L 5900:localhost:5900 -L 9091:localhost:9091 -L 8176:localhost:8176 -L 32400:localhost:32400"
+alias indigo="ssh -p 443 rein.nu -L 1176:localhost:1176"
+alias vnc="ssh -p 443 rein.nu -L 5900:localhost:5900"
+
+alias box='hostname'
